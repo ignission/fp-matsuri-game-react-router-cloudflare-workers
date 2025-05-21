3643efeabd6949249e3f86c2d2613cd83effa4d6
Shoma Nishitateno <shoma416@gmail.com>
2025-05-21 23:11:23 +0900

    https://developer.mozilla.org/ja/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Bounce_off_the_walls

--- app/game-breakout/index.tsx
+++ app/game-breakout/index.tsx
@@ -16,8 +16,8 @@ const GameBreakout: React.FC = () => {
 
 		let x = CANVAS_WIDTH / 2;
 		let y = CANVAS_HEIGHT - 30;
-		const dx = 2;
-		const dy = -2;
+		let dx = 2;
+		let dy = -2;
 
 		function drawBall() {
 			if (!ctx) return;
@@ -32,6 +32,15 @@ const GameBreakout: React.FC = () => {
 			if (!ctx) return;
 			ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 			drawBall();
+
+			// 壁との衝突判定
+			if (x + dx > CANVAS_WIDTH - BALL_RADIUS || x + dx < BALL_RADIUS) {
+				dx = -dx;
+			}
+			if (y + dy > CANVAS_HEIGHT - BALL_RADIUS || y + dy < BALL_RADIUS) {
+				dy = -dy;
+			}
+
 			x += dx;
 			y += dy;
 		}
